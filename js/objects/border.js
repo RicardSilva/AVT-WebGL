dac9c69501462be0443f8549ad221ba182540c02
Jotax <jdantasloureiro@hotmail.com>
2017-12-05 21:45:01 +0000

    border hitbox
    border hitbox

--- js/objects/border.js
+++ js/objects/border.js
@@ -6,12 +6,31 @@ function Border(position, width, length) {
 	
 	this.width = width;
 	this.length = length;
+	
+	//hitbox
+	this.minCorner;
+	this.maxCorner;
+	this.center;
+	
+	this.updateHitbox();
+	this.updateCenter();
 }
 
-Border.prototype.draw = function() {
-	
+Border.prototype.draw = function() {}
+
+Border.prototype.update = function() {}
+
+Border.prototype.updateCenter = function() {
+	this.center = vec3.fromValues(this.minCorner.x + (this.maxCorner.x - this.minCorner.x) / 2,
+							this.minCorner.y + (this.maxCorner.y - this.minCorner.y) / 2,
+							this.minCorner.z + (this.maxCorner.z - this.minCorner.z) / 2);
 }
 
-Border.prototype.update = function() {
-	
+Border.prototype.updateHitbox = function() {
+	this.minCorner = vec3.fromValues(this.position.x - this.width / 2,
+								this.position.y - this.height / 2,
+								this.position.z - this.length / 2);
+	this.maxCorner = vec3.fromValues(this.position.x + this.width / 2,
+								this.position.y + this.height / 2,
+								this.position.z + this.length / 2);
 }
